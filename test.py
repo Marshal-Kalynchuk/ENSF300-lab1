473051a1e44488e15f733f782368ee634ca6532a
Marshal <marshalkalynchuk1@gmail.com>
2022-09-23 16:26:06 -0600

    Added prints to test

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from main import *
 
 def test_eval():
+    print("Test 1 =", evaluate(['+', '/'],[1, 4, 2]))
     assert evaluate(['+', '/'],[1, 4, 2]) == 3, "Should be 3"
+    print("Test 2 =", evaluate(['+', '/'],[1, 4, 2]))
     assert evaluate(['+-', '-'],[7, 3, 2]) == 2, "Should be 2"
+    print("Test 3 =", evaluate(['+', '/'],[1, 4, 2]))
     assert evaluate(['*', '/'],[2, 2, 2]) == 2, "Should be 2"
+    print("Test 4 =", evaluate(['+', '/'],[1, 4, 2]))
     assert evaluate(['/', '-'],[6, 3, 1]) == 1, "Should be 1"
+    print("Test 5 =", evaluate(['+', '/'],[1, 4, 2]))
     assert evaluate(['/', '*'],[1, 2, 1]) == -0.5, "Should be -0.5"
 
 if __name__ == "__main__":
