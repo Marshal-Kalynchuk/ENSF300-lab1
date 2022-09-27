630742221f9bbebec70bc4c50619100cc1212373
magedelhabiby <maged.elhabiby@ucalgary.ca>
2022-09-27 10:14:55 -0600

    Testing Main and Comments for Main

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def validation(program_input, input_type, input_position, previous_input): #this
     return program_input #returns a valid value, it is a string if its an operator and a interger if it is a number.
 
 def evaluate (operations,operands):
-    
+    result = 0
     if (operations[0] == "*"):
         result = myMul(operands[0],operands[1])
         if(operations[1] == "*"):
@@ -82,7 +82,7 @@ def evaluate (operations,operands):
             result = myDiv(operands[1],operands[2])
             result = myAdd(operands[0],result)    
         else:
-            result = result + myAdd(operands[0],operands[1])
+            result =  myAdd(operands[0],operands[1])
     elif (operations[0] == "-"):
         if(operations[1] == "*"):
             result = myMul(operands[1],operands[2])
@@ -91,36 +91,36 @@ def evaluate (operations,operands):
             result = myDiv(operands[1],operands[2])
             result = mySub(operands[0],result)
         else:
-            result = result + mySub(operands[0],operands[1])
+            result = mySub(operands[0],operands[1])
     if (operations[1] == "+"):
-        result = result + myAdd(result,operands[2])  
+        result = myAdd(result,operands[2])  
     elif (operations[1] == "-"):
-        result = result + mySub(result,operands[2]) 
+        result = mySub(result,operands[2]) 
     return result
 
 def display(operations,operands,result):
     print("{} {} {} {} {} = {}".format(operands[0], operations[0], operands[1], operations[1], operands[2], result))
 
 def main():
-    first_number_input = str(input('Please enter the first number: '))
-    first_number = validation(first_number_input,0,1,0)
+    first_number_input = str(input('Please enter the first number: '))          #Takes in the input from The User
+    first_number = validation(first_number_input,0,1,0)                         #Uses the Validation Function to Validates the inputs
 
-    first_operator_input = str(input('Please enter the first operator: '))
-    first_operator = validation(first_operator_input,1,2,first_number)
+    first_operator_input = str(input('Please enter the first operator: '))      #Takes in the input from The User
+    first_operator = validation(first_operator_input,1,2,first_number)          #Uses the Validation Function to Validates the inputs   
 
-    second_number_input = str(input('Please enter the second number: '))
-    second_number = validation(second_number_input,0,3, first_operator)
+    second_number_input = str(input('Please enter the second number: '))        #Takes in the input from The User
+    second_number = validation(second_number_input,0,3, first_operator)         #Uses the Validation Function to Validates the inputs
     
-    second_operator_input = str(input('Please enter the second operator: '))
-    second_operator = validation(second_operator_input,1,4,second_number)
+    second_operator_input = str(input('Please enter the second operator: '))    #Takes in the input from The User
+    second_operator = validation(second_operator_input,1,4,second_number)       #Uses the Validation Function to Validates the inputs
 
-    third_number_input = str(input('Please enter the third number: '))
-    third_number = validation(third_number_input,0,5,second_operator)
-    operators = [first_operator, second_operator]
-    operands = [first_number, second_number, third_number]
+    third_number_input = str(input('Please enter the third number: '))          #Takes in the input from The User
+    third_number = validation(third_number_input,0,5,second_operator)           #Uses the Validation Function to Validates the inputs
+    operators = [first_operator, second_operator]                               #Creates the list for operators
+    operands = [first_number, second_number, third_number]                      #Creates the list for operands
 
-    answer = evaluate(operators, operands)
-    display(operators, operands, answer)
+    answer = evaluate(operators, operands)                                      #Uses the Evaluate Function to find the anwser
+    display(operators, operands, answer)                                        #Uses the Display Function to display the answer
 
     
 if __name__ == "__main__":
