e0af52770cc7ddf3831f82eccb474030e1c12a7f
magedelhabiby <113372487+magedelhabiby@users.noreply.github.com>
2022-10-03 16:48:06 -0600

    Add files via upload
    Comments

--- main.py
+++ main.py
@@ -35,13 +35,12 @@ def mySub(a, b):
     """ 
     return a - b
 
-def validation(program_input, input_type, input_position, previous_input): 
-    '''this function needs four inputs
-    program_input is a string parameter of the user input
-    input_type is a interger that determines if the input is supposed to be a number or a operator
-    input_position is a interger thqt represents the position of the input in the equation
-    previous_input is the verfifed input of the previous position in the equation
-    this function returns a string if the input is in the operator position or a interger if the input is in the number position''' 
+def validation(program_input, input_type, input_position, previous_input): #this function needs four inputs
+    #program_input is a string parameter of the user input
+    #input_type is a interger that determines if the input is supposed to be a number or a operator
+    #input_position is a interger thqt represents the position of the input in the equation
+    #previous_input is the verfifed input of the previous position in the equation
+    #this function returns a string if the input is in the operator position or a interger if the input is in the number position 
 
     input_valid = False #used in checking validity of input
     operators_list = ["*","/","-","+"] #lsits of valid responses
@@ -64,88 +63,93 @@ def validation(program_input, input_type, input_position, previous_input):
                     input_valid = False #returns the validity statement to false in the case that input_valid became true
 
         if input_position in [3,5]:
-            if input_position == 3 and previous_input == "/" and program_input == "0" and input_valid == True: #checks if previous input is a division
-                print("\nYour input is invalid. You cannot divide by zero.")
-                program_input = str(input("Please enter a valid number: ")) #requests an input
-                input_valid = False
-            if input_position == 5 and previous_input == "/" and program_input == "0" and input_valid == True:#checks if previous input is a division
-                print("\nYour input is invalid. You cannot divide by zero.")
-                program_input = str(input("Please enter a valid number: ")) #requests an input
-                input_valid = False
-        elif input_valid == False and input_type == 0: #if the number is not valid the program asks for more input
-            print("\nYour input is invalid. Please input a number.")
+            while program_input == "0":
+                if input_position == 3 and previous_input == "/" and program_input == "0" and input_valid == True:
+                    print("Your input is invalid. You cannot divide by zero.\n")
+                    program_input = input("Please enter a valid number: ") #requests an input
+                    input_valid = False
+                if input_position == 5 and previous_input == "/" and program_input == "0" and input_valid == True:
+                    print("Your input is invalid. You cannot divide by zero.\n")
+                    program_input = input("Please enter a valid number: ") #requests an input
+                    input_valid = False
+                else:
+                    break
+
+        if input_valid == False and input_type == 0: #if the number is not valid the program asks for more input
+            print("Your input is invalid. Please input a number.\n")
             program_input = input("Please enter a valid number: ") #requests an input
-        elif input_valid == False and input_type ==1: #if the operator is not valid the program asks for more input
-            print("\nYour input is invalid. Please input an operator.")
+        if input_valid == False and input_type ==1: #if the operator is not valid the program asks for more input
+            print("Your input is invalid. Please input an operator.\n")
             program_input = input("Please enter a valid operator: ") #requests an input       
 
-    if input_valid == True and input_type == 0: #sets the program_input as an integer if its a digit
-        program_input = int(program_input) 
+    if input_valid == True and input_type == 0: #sets the program_input as an interger if its a digit
+        program_input = int(program_input)
 
-    return program_input #returns a valid value, it is a string if its an operator and a integer if it is a number.
+    return program_input #returns a valid value, it is a string if its an operator and a interger if it is a number.
 
 def evaluate (operations,operands):
     result = 0
-    if (operations[0] == "*"):
-        result = myMul(operands[0],operands[1])
-        if(operations[1] == "*"):
-            result =  myMul(result,operands[2])
-        elif(operations[1] == "/"):
-            result =  myDiv(result,operands[2])
-    elif (operations[0] == "/"):
-        result = myDiv(operands[0],operands[1])
-        if(operations[1] == "*"):
-            result =  myMul(result,operands[2])
-        elif(operations[1] == "/"):
-            result = myDiv(result,operands[2]) 
+    if (operations[0] == "*"):                      #If statement to see if the first operator is *
+        result = myMul(operands[0],operands[1])     #Uses the myMul function
+        if(operations[1] == "*"):                   #If statement to see if the second operator is *
+            result =  myMul(result,operands[2])     #Uses the myMul function
+        elif(operations[1] == "/"):                 #If else statement to see if the second operator is /
+            result =  myDiv(result,operands[2])     #Uses the myDiv function
+    elif (operations[0] == "/"):                    #If else statement to see if the first operator is /
+        result = myDiv(operands[0],operands[1])     #Uses the myDiv function
+        if(operations[1] == "*"):                   #If statement to see if the second operator is *
+            result =  myMul(result,operands[2])     #Uses the myMul function
+        elif(operations[1] == "/"):                 #If else statement to see if the second operator is /
+            result = myDiv(result,operands[2])      #Uses the myDiv function
 
-    elif (operations[0] == "+"):
-        if(operations[1] == "*"):
-            result = myMul(operands[1],operands[2])
-            result = myAdd(operands[0],result)
-        elif(operations[1] == "/"):
-            result = myDiv(operands[1],operands[2])
-            result = myAdd(operands[0],result)    
-        else:
-            result = myAdd(operands[0],operands[1])
-    elif (operations[0] == "-"):
-        if(operations[1] == "*"):
-            result = myMul(operands[1],operands[2])
-            result = mySub(operands[0],result)
-        elif(operations[1] == "/"):
-            result = myDiv(operands[1],operands[2])
-            result = mySub(operands[0],result)
-        else:
-            result = mySub(operands[0],operands[1])
-    if (operations[1] == "+"):
-        result = myAdd(result,operands[2])  
-    elif (operations[1] == "-"):
-        result = mySub(result,operands[2]) 
+    elif (operations[0] == "+"):                    #If statement to see if the first operator is +
+        if(operations[1] == "*"):                   #If statement to see if the second operator is *
+            result = myMul(operands[1],operands[2]) #Uses the myMul function
+            result = myAdd(operands[0],result)      #Uses the myAdd function
+        elif(operations[1] == "/"):                 #If else statement to see if the second operator is /
+            result = myDiv(operands[1],operands[2]) #Uses the myDiv function
+            result = myAdd(operands[0],result)      #Uses the myAdd function
+        else:                                       
+            result =  myAdd(operands[0],operands[1])#Uses the myAdd function
+    elif (operations[0] == "-"):                    #If else statement to see if the first operator is -
+        if(operations[1] == "*"):                   #If statement to see if the second operator is *
+            result = myMul(operands[1],operands[2]) #Uses the myMul function
+            result = mySub(operands[0],result)      #Uses the mySub function
+        elif(operations[1] == "/"):                 #If else statement to see if the second operator is /
+            result = myDiv(operands[1],operands[2]) #Uses the myDiv function
+            result = mySub(operands[0],result)      #Uses the mySub function
+        else:                                       
+            result = mySub(operands[0],operands[1]) #Uses the mySub function
+    if (operations[1] == "+"):                      #If statement to see if the second operator is +
+        result = myAdd(result,operands[2])          #Uses the myAdd function
+    elif (operations[1] == "-"):                    #If else statement to see if the second operator is -
+        result = mySub(result,operands[2])          #Uses the mySub function
     return result
 
 def display(operations,operands,result):
-    print("{} {} {} {} {} = {}".format(operands[0], operations[0], operands[1], operations[1], operands[2], result))
+    print("{} {} {} {} {} = {}".format(operands[0], operations[0], operands[1], operations[1], operands[2], result)) #Prints the formatted display
 
 def main():
+    print("Welcome to the simple calculator. Please enter three digits and two operators into the calculator:")
     first_number_input = str(input('Please enter the first number: '))
     first_number = validation(first_number_input,0,1,0)
 
-    first_operator_input = str(input('Please enter the first operator: '))
-    first_operator = validation(first_operator_input,1,2,first_number)
+    first_operator_input = str(input('Please enter the first operator: '))      #Takes in the input from The User
+    first_operator = validation(first_operator_input,1,2,first_number)          #Uses the Validation Function to Validates the inputs   
 
-    second_number_input = str(input('Please enter the second number: '))
-    second_number = validation(second_number_input,0,3, first_operator)
+    second_number_input = str(input('Please enter the second number: '))        #Takes in the input from The User
+    second_number = validation(second_number_input,0,3, first_operator)         #Uses the Validation Function to Validates the inputs
     
-    second_operator_input = str(input('Please enter the second operator: '))
-    second_operator = validation(second_operator_input,1,4,second_number)
+    second_operator_input = str(input('Please enter the second operator: '))    #Takes in the input from The User
+    second_operator = validation(second_operator_input,1,4,second_number)       #Uses the Validation Function to Validates the inputs
 
-    third_number_input = str(input('Please enter the third number: '))
-    third_number = validation(third_number_input,0,5,second_operator)
-    operators = [first_operator, second_operator]
-    operands = [first_number, second_number, third_number]
+    third_number_input = str(input('Please enter the third number: '))          #Takes in the input from The User
+    third_number = validation(third_number_input,0,5,second_operator)           #Uses the Validation Function to Validates the inputs
+    operators = [first_operator, second_operator]                               #Creates the list for operators
+    operands = [first_number, second_number, third_number]                      #Creates the list for operands
 
-    answer = evaluate(operators, operands)
-    display(operators, operands, answer)
+    answer = evaluate(operators, operands)                                      #Uses the Evaluate Function to find the anwser
+    display(operators, operands, answer)                                        #Uses the Display Function to display the answer
 
     
 if __name__ == "__main__":
